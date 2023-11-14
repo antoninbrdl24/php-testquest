5dc8e5e66ff86bbbc3a2e665c51aee4c3d1876fa
antoninbrdl24 <antonin.brondel24@gmail.com>
2023-11-14 23:41:23 +0100

    test complets

--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,34 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $firstValue = 5;
+        $secondValue = 8;
+        $expectedResult = 40;
+
+        //Act
+        $calculator = new Calculator();
+        $multiple = $calculator->multiplication($firstValue, $secondValue);
+        
+        //Assert
+        $this->assertSame($expectedResult, $multiple, 
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+    public function testIsEven(): void
+    {
+        //Arrange
+        $firstValue = 4;
+        
+        //Act
+        $calculator = new Calculator();
+        $isEven = $calculator->isEven($firstValue);
+        
+        //Assert
+        $this->assertTrue($isEven,
+            "La méthode isEven() ne fonctionne plus :-("
+        );
+    }
 }
